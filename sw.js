a1308ac14f819d1d34f9c4adce88bc63e13084cd
volvimen <copa.pe.dh@gmail.com>
2025-12-31 12:28:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,15 @@
-const CACHE_NAME = 'filhao-v8';
+const CACHE_NAME = 'filhao-v9';
 const ASSETS = [
   './',
   './index.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+  'https://cdn-icons-png.flaticon.com/512/3616/3616230.png'
 ];
 
-// Instalação e Cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// Responde com Cache ou busca na Rede
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
